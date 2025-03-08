91ab2fcccef67ddf5b24d05f502595e75677d328
Jonas Keller <jonas.keller1@mailbox.tu-dresden.de>
2025-03-08 17:17:45 +0100

    image preprocessor for reading, cropping, grayscale and rotation

---
+++ src/image_preprocessor.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import os
+
+
+class ImagePreprocessor:
+    def __init__(self, image: np.ndarray):
+        self.image = image
+
+    def crop_to_roi(self):
+        self.image = self.image[490:630, 925:1235]
+
+    def rgb_to_grayscale(self):
+        self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+
+    def rotate(self):
+        self.image = cv2.rotate(self.image, cv2.ROTATE_180)
+
+
+def read_image_from_file(PATH: str) -> np.ndarray:
+    return cv2.imread(PATH_TO_IMAGE)
+
+
+if __name__ == "__main__":
+    PATH = "/home/jonas/coding_practice/image_processing_practice/img"
+    PATH_TO_IMAGE = os.path.join(PATH, "3d_printer.png")
+
+    raw_image = read_image_from_file(PATH_TO_IMAGE)
+    image = ImagePreprocessor(raw_image)
+    image.crop_to_roi()
+    image.rgb_to_grayscale()
+    image.rotate()
+
+    window_name = "Image 3D-printer"
+    cv2.imshow(window_name, image.image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
