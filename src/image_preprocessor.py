c8b989e4ee34ddd4f3d398976cd8907acae87fcf
Jonas Keller <jonas.keller1@mailbox.tu-dresden.de>
2025-03-11 18:06:30 +0100

    clean up

--- src/image_preprocessor.py
+++ src/image_preprocessor.py
@@ -7,6 +7,9 @@ class ImagePreprocessor:
     def __init__(self, image: np.ndarray):
         self.image = image
 
+    def get_image(self) -> np.ndarray:
+        return self.image
+
     def crop_to_roi(self) -> None:
         self.image = self.image[490:630, 925:1235]
 
@@ -15,9 +18,6 @@ class ImagePreprocessor:
 
     def rotate(self) -> None:
         self.image = cv2.rotate(self.image, cv2.ROTATE_180)
-    
-    def get_image(self) -> np.ndarray:
-        return self.image
 
 
 def read_image_from_file(PATH: str) -> np.ndarray:
@@ -25,7 +25,7 @@ def read_image_from_file(PATH: str) -> np.ndarray:
 
 
 if __name__ == "__main__":
-    PATH = "/home/jonas/coding_practice/image_processing_practice/img"
+    PATH = "../img"
     PATH_TO_IMAGE = os.path.join(PATH, "3d_printer.png")
 
     raw_image = read_image_from_file(PATH_TO_IMAGE)
