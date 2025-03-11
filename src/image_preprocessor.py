1a20e03ea4c046d6482a990d3ee9962241f89b49
Jonas Keller <jonas.keller1@mailbox.tu-dresden.de>
2025-03-11 16:22:16 +0100

    added return types

--- src/image_preprocessor.py
+++ src/image_preprocessor.py
@@ -7,13 +7,13 @@ class ImagePreprocessor:
     def __init__(self, image: np.ndarray):
         self.image = image
 
-    def crop_to_roi(self):
+    def crop_to_roi(self) -> None:
         self.image = self.image[490:630, 925:1235]
 
-    def rgb_to_grayscale(self):
+    def rgb_to_grayscale(self) -> None:
         self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 
-    def rotate(self):
+    def rotate(self) -> None:
         self.image = cv2.rotate(self.image, cv2.ROTATE_180)
     
     def get_image(self) -> np.ndarray:
@@ -21,7 +21,7 @@ class ImagePreprocessor:
 
 
 def read_image_from_file(PATH: str) -> np.ndarray:
-    return cv2.imread(PATH_TO_IMAGE)
+    return cv2.imread(PATH)
 
 
 if __name__ == "__main__":
