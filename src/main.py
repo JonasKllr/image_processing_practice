8b2307f29f4c50ddf485aa6e8ddb8c0320c42bcd
Jonas Keller <jonas.keller1@mailbox.tu-dresden.de>
2025-03-11 18:06:07 +0100

    relative path

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import cv2
 import os
+from matplotlib import pyplot as plt
 
 from image_preprocessor import ImagePreprocessor, read_image_from_file
 from point_operators import PointOperators
 
-PATH = "/home/jonas/coding_practice/image_processing_practice/img"
+PATH = "../img"
 PATH_TO_IMAGE = os.path.join(PATH, "3d_printer.png")
 
 raw_image = read_image_from_file(PATH_TO_IMAGE)
